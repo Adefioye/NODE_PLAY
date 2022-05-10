d46cbc517fa3b4ff2e4349790c4bd13264c47e42
KokoLuciferus <abdulhakeemadefioye@gmail.com>
2022-05-10 11:35:22 +0100

    Chained multiple middleware

--- controllers/tourControllers.js
+++ controllers/tourControllers.js
@@ -17,6 +17,16 @@ exports.checkID = (req, res, next, val) => {
   next();
 };
 
+exports.checkBody = (req, res, next) => {
+  if (!req.body.name || !req.body.price) {
+    return res.status(400).json({
+      status: 'Fail',
+      message: 'No name or price property on request body',
+    });
+  }
+  next();
+};
+
 exports.getAllTours = (req, res) => {
   res.status(200).json({
     status: 'Success',
