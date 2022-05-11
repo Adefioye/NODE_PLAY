e72f52de69ab2b57ba825de9619988de95a4dc0e
KokoLuciferus <abdulhakeemadefioye@gmail.com>
2022-05-11 15:17:22 +0100

    Fiddling with mongoose

--- app.js
+++ app.js
@@ -1,21 +1,29 @@
-const express = require('express');
-const morgan = require('morgan');
+const express = require("express");
+const morgan = require("morgan");
 
-const tourRouter = require('./routes/tourRoutes');
-const userRouter = require('./routes/userRoutes');
+const tourRouter = require("./routes/tourRoutes");
+const userRouter = require("./routes/userRoutes");
 
 const app = express();
 
-app.use(morgan('dev'));
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
-  console.log('Hello middleware');
+  console.log("Hello middleware");
   next();
 });
 
-app.use('/api/v1/tours', tourRouter);
-app.use('/api/v1/users', userRouter);
+app.use("/api/v1/tours", tourRouter);
+app.use("/api/v1/users", userRouter);
 
 module.exports = app;
+
+// {
+//   "singleQuote": false
+// }
--- server.js
+++ server.js
@@ -1,6 +1,66 @@
-const app = require('./app');
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 
-PORT = 5000;
+dotenv.config({ path: "./config.env" });
+
+const app = require("./app");
+
+const MONGO_URL = process.env.MONGO_URL.replace(
+  "<PASSWORD>",
+  process.env.MONGO_PASSWORD
+);
+
+// CONNECT TO DATABASE
+mongoose
+  .connect(MONGO_URL, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Database is connected!");
+  })
+  .catch((err) => {
+    console.log("Error connecting to database!");
+    console.log(err);
+  });
+
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "A tour must have a name"],
+    unique: true,
+  },
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+  price: {
+    type: Number,
+    required: [true, "A tour must have a price"],
+  },
+});
+
+const Tour = mongoose.model("Tour", tourSchema);
+
+// const newTour = new Tour({
+//   name: "The Park Camper",
+//   price: 970,
+// });
+
+// newTour
+//   .save()
+//   .then((data) => {
+//     console.log("Data saved into database");
+//     console.log(data);
+//   })
+//   .catch((err) => {
+//     console.log("Error connecting to database!");
+//     console.log(err);
+//   });
+
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`App listening on PORT: ${PORT}`);
