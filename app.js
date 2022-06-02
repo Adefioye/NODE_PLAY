5805dbc601175ebfe421a4169f91e1f3cb1178f0
KokoLuciferus <abdulhakeemadefioye@gmail.com>
2022-06-02 14:29:14 +0100

    Added mongo sanitize and xss-clean as middleware to prevent NOSQL and XSS attack

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const morgan = require("morgan");
 const rateLimit = require("express-rate-limit");
+const helmet = require("helmet");
+const mongoSanitize = require("express-mongo-sanitize");
+const xss = require("xss-clean");
 
 const AppError = require("./utils/appError");
 const tourRouter = require("./routes/tourRoutes");
@@ -9,6 +12,10 @@ const globalErrorHandler = require("./controllers/errorController");
 
 const app = express();
 
+// Set security HTTP headers
+app.use(helmet());
+
+// Logging middleware for development environment
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
@@ -20,11 +27,22 @@ const limiter = rateLimit({
   message: "Too many requests made to this API. Please try again in an hour",
 });
 
+// Limit number of request sent to the API endpoints
 app.use("/api", limiter);
-app.use(express.json());
+
+// Body parser middleware: Pass body to req.body
+app.use(express.json({ limit: "20kb" }));
 app.use(express.urlencoded({ extended: true }));
+
+// Data sanitization for preventing NOSQL injections query
+app.use(mongoSanitize());
+// Data sanitization for preventing XSS attack
+app.use(xss());
+
+// Middleware for serving static assets like CSS, HTML and image assets
 app.use(express.static(`${__dirname}/public`));
 
+// Middlware for handling requests to users and tours endpoints
 app.use("/api/v1/tours", tourRouter);
 app.use("/api/v1/users", userRouter);
 
@@ -41,6 +59,7 @@ app.all("*", (req, res, next) => {
   next(err);
 });
 
+// Global error handler middleware
 app.use(globalErrorHandler);
 
 module.exports = app;
